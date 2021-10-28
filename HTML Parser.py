962afa9c73a05a1bef4555779b08a470efbe6977
Meysiolio <saturninsky@gmail.com>
2021-10-28 20:02:17 +0330

    edit code and md file #3

--- HTML Parser.py
+++ HTML Parser.py
@@ -1,7 +1,7 @@
 from html.parser import HTMLParser
 
 # create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
+class MyHTMLParser1(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print("Start :", tag)
         if attrs:
@@ -15,12 +15,33 @@ class MyHTMLParser(HTMLParser):
             for attr in attrs:
                 print("-> " + str(attr[0]) + " > " +  str(attr[1]))
 
+class MyHTMLParser2(HTMLParser):
+    def handle_comment(self, data):
+        if '\n' in data:
+            print(">>> Multi-line Comment\n" + data)
+        else:
+            print(">>> Single-line Comment\n" + data)
+    def handle_data(self, data):
+        if data.strip():
+            print(">>> Data\n" + data)
+
+
 if __name__ == '__main__':
-    input_html = []
-    parser = MyHTMLParser() # instantiate the parser and fed it some HTML
+    if int(input('Parser1 or Parser2?!')) == 1:
+        input_html = []
+        parser = MyHTMLParser1() # instantiate the parser and fed it some HTML
 
-    for _ in range(int(input())):
-        input_html.append(input())
+        for _ in range(int(input())):
+            input_html.append(input())
 
-    for i in input_html:
-        parser.feed(i)
+        for i in input_html:
+            parser.feed(i)
+    else:
+        html = ""
+        for i in range(int(input())):
+            html += input().rstrip()
+            html += '\n'
+    
+        parser = MyHTMLParser2()
+        parser.feed(html)
+        parser.close()
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Task
+## Task1 (Parser1)
 
 You are given an HTML code snippet of ***N*** lines.  
 Your task is to print start tags, end tags and empty tags separately.  
@@ -58,4 +58,54 @@ End   : h1
 Empty : br
 End   : body
 End   : html
+```
+
+## Task2 (Parser2)
+
+You are given an HTML code snippet of ***N*** lines.  
+Your task is to print the single-line comments, multi-line comments and the data.  
+
+Print the result in the following format:  
+```
+>>> Single-line Comment  
+Comment
+>>> Data                 
+My Data
+>>> Multi-line Comment  
+Comment_multiline[0]
+Comment_multiline[1]
+>>> Data
+My Data
+>>> Single-line Comment:  
+```
+**Note:** Do not print data if data == '\n'.  
+
+**Input Format**
+
+The first line contains integer ***N***, the number of lines in the HTML code snippet.  
+The next ***N*** lines contains HTML code.  
+
+**Output Format**
+
+Print the single-line comments, multi-line comments and the data in order of their occurrence from top to bottom in the snippet.  
+
+Format the answers as explained in the problem statement.  
+
+**Sample Input**
+```
+4
+<!--[if IE 9]>IE9-specific content
+<![endif]-->
+<div> Welcome to HackerRank</div>
+<!--[if IE 9]>IE9-specific content<![endif]-->
+```
+**Sample Output**
+```
+>>> Multi-line Comment
+[if IE 9]>IE9-specific content
+<![endif]
+>>> Data
+ Welcome to HackerRank
+>>> Single-line Comment
+[if IE 9]>IE9-specific content<![endif]
 ```
